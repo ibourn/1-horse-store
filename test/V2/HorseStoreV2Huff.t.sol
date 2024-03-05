e000fdcace486715abf21bb111789626f48adfc8
ibourn <igor.bournazel@orange.fr>
2024-03-05 23:55:57 +0100

    :sparkles: HorseStoreV2 (solidity & Huff) & tests

---
+++ test/V2/HorseStoreV2Huff.t.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import {Base_TestV2, HorseStore} from "./Base_TestV2.t.sol";
+import {HuffDeployer} from "foundry-huff/HuffDeployer.sol";
+
+contract HorseStoreV2Huff is Base_TestV2 {
+    string public constant horseStoreLocation = "horseStoreV2/HorseStore";
+
+    function setUp() public override {
+        // args of constructor are passed in with_args
+        horseStore = HorseStore(
+            HuffDeployer.config().with_args(bytes.concat(abi.encode(NFT_NAME), abi.encode(NFT_SYMBOL))).deploy(
+                horseStoreLocation
+            )
+        );
+    }
+}
